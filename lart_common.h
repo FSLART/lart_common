798f37a4a990d85f6c3b4819a526629bd3a44e24
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-06-12 12:26:43 +0100

    implement powertrain conversions

--- lart_common.h
+++ lart_common.h
@@ -6,18 +6,30 @@
 // --- DYNAMICS CONVERSIONS ---
 
 // The radius of the tire in meters
-#define TIRE_RADIUS_M 0.255
+#define TIRE_RADIUS_M 0.255f
 
 // The number of pulses per revolution
 #define PULSES_PER_REVOLUTION 37
 
 // The diameter of the tire in meters
-#define TIRE_DIAMETER_M (2 * M_PI * TIRE_RADIUS_M)
+#define TIRE_PERIMETER_M (2 * M_PI * TIRE_RADIUS_M)
 
 // The distance traveled by pulse
-#define METERS_PER_PULSE (TIRE_DIAMETER_M / PULSES_PER_REVOLUTION)
+#define METERS_PER_PULSE (TIRE_PERIMETER_M / PULSES_PER_REVOLUTION)
 
 // Convert the pulse count difference to traveled distance in meters
 #define PULSE_DIFF_TO_DISTANCE_M(pulse_diff) (pulse_diff * METERS_PER_PULSE)
+
+
+// --- POWERTRAIN CONVERSIONS ---
+
+// Transmission ratio
+#define TRANSMISSION_RATIO 4.0f
+
+// Meters per second to engine revolutions per minute
+#define MS_TO_RPM(ms) (TRANSMISSION_RATIO * (MS / TIRE_PERIMETER_M) * 60.0)
+
+// Engine revolutions per minute to meters per second
+#define RPM_TO_MS(rpm) (TIRE_PERIMETER_M * (rmp / TRANSMISSION_RATIO / 60.0))
 
 # endif  // LART_COMMON_H_
